046568d336116031a77932b4ef53c323a304ad04
century90 <century90@mail.ru>
2022-11-02 16:22:34 +0300

    commit for errow

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -16,6 +16,6 @@ public class PostmanEchoTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("some info"));
     }
 }
